512e1f3661de9bdc6f6c84ac74b829f8e0b083c9
RL <robin.lambert@hotmail.fr>
2016-07-13 15:33:50 +0200

    nowrap fix

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ $(document).ready(function(){
 		    $('#wiki').append(readData);
 		    //boucle for i<10 (pas propre...), si "nowrap" : balancer une version modifiée de la ligne suivante qui inclus un .children de plus
 		    for (i = 0; i<10; i++){
-		    	if( ($("[title='" + title_value + "']")[0].parentElement.parentElement.children[1].children) == (<span class="nowrap">) ){
-		    		console.log("nowrap found");
-				}	
+		  //   	if( ($("[title='" + title_value + "']")[0].parentElement.parentElement.children[1].children) == (<span class="nowrap">) ){
+		  //   		console.log("nowrap found");
+				// }	
 		    } 
 		    $('#result').append("En " +year+" le gagnant du Tour de France était " + $("[title='" + title_value + "']")[0].parentElement.parentElement.children[1].children[1].text + ", le meilleur grimpeur "+$("[title='" + title_value + "']")[0].parentElement.parentElement.children[6].children[1].text + " et le meilleur sprinter "+$("[title='" + title_value + "']")[0].parentElement.parentElement.children[7].children[1].text) +".";
 		  };
