25e145d8e97824523d78251845d40b193d7ae283
RL <robin.lambert@hotmail.fr>
2016-07-13 19:02:09 +0200

    wrap fix

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
 			
 		    // var gagnant... if grimpeur.text = undefined, 
 		    var gagnant = $("[title='" + title_value + "']")[0].parentElement.parentElement.children[1].children[1];
-		    if (gagnant.text == "undefined"){
+		    if (gagnant == undefined){
 		    	gagnant = gagnant.parentElement.children[0].children[1];
 		    }
 		    var grimpeur = $("[title='" + title_value + "']")[0].parentElement.parentElement.children[6].children[1];
