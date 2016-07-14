92f860a39d94c1a7150b8d0b77b828447bd5d29f
RL <robin.lambert@hotmail.fr>
2016-07-14 17:23:29 +0200

    add exceptions WWI WWII Armstrong

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ $(document).ready(function(){
 	  var year = $('#year').val();
 	  if (year < 1903 || year > 2015) {
 	  	$('#result').append("L'année entrée doit être comprise entre 1903 (première année du Tour) et 2015");
+	  } else if (year === 1915 || year === 1916 || year === 1917 || year === 1918 ||year === 1940 || year === 1941 || year === 1942 || year === 1943 || year === 1944 || year === 1945 || year === 1946){
+	  	$('#result').append("Entre 1915 et 1918, puis entre 1940 et 1946, les deux guerres mondiales ont malheureusement empêché la tenue du Tour de France.");
 	  } else {
 		  	var wikipediaHTMLResult = function(data) {
 		    	var readData = $('<div>' + data.parse.text["*"] + '</div>');
@@ -14,14 +16,17 @@ $(document).ready(function(){
 		     
 			
 		    // var gagnant... if grimpeur.text = undefined, 
+		    var gagnant_flag = true;
 		    var grimpeur_flag = true;
 		    var sprinter_flag = true;
 
+		    if (year === 1999 || year === 2000 || year === 2001 || year === 2002 || year === 2003 || year === 2004 || year === 2005 || ){ // exception pour les années où le titre a été retiré à Armstrong
+		    	gagnant_flag = false; 
+	  		}
 
 
 		    var gagnant = $("[title='" + title_value + "']")[0].parentElement.parentElement.children[1].children[1];
-		    if (year === "1959"){
-		    	console.log("boucle atteinte");
+		    if (year === "1959"){ // exception : le nom du gagnan est sous une balise (nowrap) supplémentaire cette année là
 		    	gagnant = $("[title='Tour de France 1959']")[0].parentElement.parentElement.children[1].children[0].children[1];
 		    }
 
@@ -36,13 +41,18 @@ $(document).ready(function(){
 		    }
 
 		    // css bootstrap pour faire plus joli bootstrap.css ou bootstrap.style.css
-		    if (grimpeur_flag && sprinter_flag){
+		    if (grimpeur_flag && sprinter_flag && grimpeur_flag){
 				$('#result').append("En " +year+" le gagnant du Tour de France était " + gagnant.text + ", le meilleur grimpeur "+grimpeur.text + " et le meilleur sprinter "+sprinter.text +".");
+
+		    } else if (grimpeur_flag && sprinter_flag && !grimpeur_flag){
+				$('#result').append("En " +year+" le titre de gagnant du Tour de France de Lance Armstrong a été révoqué par l'union cycliste internationale pour dopage, le meilleur grimpeur était  "+grimpeur.text + " et le meilleur sprinter "+sprinter.text +".");
 
 		    } else if (grimpeur_flag && !sprinter_flag){
 				$('#result').append("En " +year+" le gagnant du Tour de France était " + gagnant.text + " et le meilleur grimpeur "+grimpeur.text + ". Il n'y avait pas encore de meilleur sprinter cette année, le premier maillot vert datant de 1953.");
+
 		    } else {
 		    	$('#result').append("En " +year+" le gagnant du Tour de France était " + gagnant.text + ". Il n'y avait pas de meilleur grimpeur puisque le premier maillot à pois date de 1953. Idem pour le maillot vert de meilleur sprinter, qui date lui de 1933.");
+
 
 		    }
 		  };
