bd6a8a80dec7a534ef3f424ce3d2ee4be1154c3b
RL <robin.lambert@hotmail.fr>
2016-07-14 18:23:09 +0200

    bootstrap button

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ $(document).ready(function(){
 
 	$('#submit-button').click(function() {
 	  $('#result').empty();
+	  
 	  var year = $('#year').val();
+
 	  if (year < 1903 || year > 2015) {
 	  	$('#result').append("L'année entrée doit être comprise entre 1903 (première année du Tour) et l'année du dernier tour de France");
+
 	  } else if ((year >1914 && year <1919) || (year >1939 || year < 1947)){
-	  			// ((year >1914 && year <1919) || (year >1939 || year < 1947))
-	  			//(year == 1915 || year == 1916 || year == 1917 || year == 1918 || year == 1940 || year == 1941 || year == 1942 || year == 1943 || year == 1944 || year == 1945 || year == 1946)
-	  	console.log ("boucle entrée");
 	  	$('#result').append("Entre 1915 et 1918, puis entre 1940 et 1946, les deux guerres mondiales ont malheureusement empêché la tenue du Tour de France.");
+
 	  } else {
 		  	var wikipediaHTMLResult = function(data) {
 		    	var readData = $('<div>' + data.parse.text["*"] + '</div>');
